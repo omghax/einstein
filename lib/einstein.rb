5035c306f82f7e7d99a7d7b127251659551971d0
Dray Lacy <dray@izea.com>
2008-04-24 21:14:38 -0400

    Added a couple simple facade methods to the base Einstein module, so you can easily jump right in and use Einstein without needing to know anything about its internals.

--- lib/einstein.rb
+++ lib/einstein.rb
@@ -4,5 +4,17 @@ require "einstein/parser"
 require "einstein/errors"
 
 module Einstein
-  
+  class << self
+    def parse(expression)
+      Parser.new.parse(expression)
+    end
+
+    def evaluate(expression, scope = {})
+      parse(expression).evaluate(scope)
+    end
+
+    def sexp(expression)
+      parse(expression).to_sexp
+    end
+  end
 end
