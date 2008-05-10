2b7ec711a506ace5d0b681af7e0ca1633624104c
Dray Lacy <dray@izea.com>
2008-05-09 23:43:04 -0400

    Documented Einstein::Expression.

--- lib/einstein/expression.rb
+++ lib/einstein/expression.rb
@@ -2,23 +2,36 @@ require 'einstein/processors/evaluate_processor'
 require 'einstein/processors/pretty_print_processor'
 
 module Einstein
+  # Node representing an entire Einstein statement.  This is the root-level
+  # node of the parser.  This node's +exp+ is the tree of expressions that
+  # make up a single logical statement.
   class Expression
-    def initialize(exp)
-      @exp = exp
+    # Initialize a new expression wrapping the given +sexp+, which is expected
+    # to be an s-expression represented as a set of nested arrays.
+    # 
+    # Example:
+    #   Expression.new([:lit, 10]) # => 10
+    #   Expression.new([:multiply, [:lit, 10], [:lit, 5]]) # => (10 * 5)
+    def initialize(sexp)
+      @sexp = sexp
     end
 
-    def to_sexp
-      @exp
-    end
+    # The s-expression given to this Expression's constructor.
+    attr_reader :sexp
+    alias_method :to_sexp, :sexp
 
+    # Evaluate this node against the given +scope+. Returns a numeric value
+    # calculated by walking the AST with an instance of EvaluateProcessor.
     def evaluate(scope = {})
       EvaluateProcessor.new(scope).process(@exp)
     end
 
+    # Performs a "pretty print" of this expression.
     def to_s
       PrettyPrintProcessor.new.process(@exp)
     end
 
+    # Also use #to_s for inspecting a node in IRB.
     alias_method :inspect, :to_s
   end
 end
